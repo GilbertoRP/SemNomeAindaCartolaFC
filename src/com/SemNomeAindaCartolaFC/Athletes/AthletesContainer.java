0827837249123e2466ef3ce25f9e4f9dca17a228
gilberto <grprosa@inf.ufrgs.br>
2016-09-27 10:41:39 -0300

    Add: AthletesContainer, working with sorted and classified algorithms.

---
+++ src/com/SemNomeAindaCartolaFC/Athletes/AthletesContainer.java
@@ -0,0 +1,41 @@
+package com.SemNomeAindaCartolaFC.Athletes;
+
+import java.util.ArrayList;
+
+public class AthletesContainer {
+	
+	private String savePath;
+	
+	private ArrayList<Athlete> athletes = new ArrayList<Athlete>();
+
+	public Athlete[] getAthletes() {
+		return (Athlete[]) this.athletes.toArray().clone();
+	}
+	
+	public void setTimeSavePath(String savePath) {
+		this.savePath = savePath;
+	}
+	
+	public void addAthlete(Athlete athlete) {
+		athletes.add(athlete);
+	}
+	
+	public AthletesContainer sortWith(SortAthletesAlgorithm sortAlgorithm) {
+		Athlete[] sortedAthletes = sortAlgorithm.sort(this.getAthletes());
+		this.replaceAllCurrentAthletesWith(sortedAthletes);		
+		return this;
+	}
+	
+	public AthletesContainer classifyByProperty(ClassifyAthletesAlgorithm classifyAlgorithm) {
+		Athlete[] classifiedAthletes = classifyAlgorithm.classify(this.getAthletes());
+		this.replaceAllCurrentAthletesWith(classifiedAthletes);
+		return this;
+	}
+	
+	private void replaceAllCurrentAthletesWith(Athlete[] newAthletes) {
+		for(int i = 0; i < newAthletes.length; i++) {
+			Athlete athleteAtIthIndex = newAthletes[i];
+			this.athletes.set(i, athleteAtIthIndex);
+		}
+	}
+}

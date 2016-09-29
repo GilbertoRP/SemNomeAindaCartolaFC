cbf963c52e66d204390e2f0a5f74345e40886110
gilberto <grprosa@inf.ufrgs.br>
2016-09-28 22:02:27 -0300

    Add: Json parser for json file

--- src/com/SemNomeAindaCartolaFC/Athletes/AthleteGenerator.java
+++ src/com/SemNomeAindaCartolaFC/Athletes/AthleteGenerator.java
@@ -1,5 +1,16 @@
 package com.SemNomeAindaCartolaFC.Athletes;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.*;
+import java.util.Optional;
+import java.util.Random;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import org.json.*;
+
 public class AthleteGenerator {
 	
 	private Double minMean = 0.0;
@@ -9,6 +20,7 @@ public class AthleteGenerator {
 	private Double minPrice = 0.0;
 	private Double maxPrice = 0.0;
 	
+	private String baseFullPath = "";
 	
 	public AthleteGenerator() {
 		
@@ -18,6 +30,10 @@ public class AthleteGenerator {
 		Athlete genAthlete = new Athlete();
 		
 		//TODO: Implement algorithm to generate random properties to athlete
+		
+		Random rand = new Random();
+		
+//		rand.net
 		
 		return genAthlete;
 	}
@@ -36,4 +52,36 @@ public class AthleteGenerator {
 		this.minPrice = min;
 		this.maxPrice = max;
 	}
+	
+	public void setBaseFullPath(String jsonFilePath) {
+		this.baseFullPath = jsonFilePath;
+	}
+	
+	private void extractMinAndMaxFromBase() {
+		Path filePath = Paths.get(this.baseFullPath);
+		try {
+			Stream<String> lines = Files.lines(filePath);
+			String jsonString = lines.reduce((line, lastLine) -> line + lastLine).get();			
+			parseJsonString(jsonString);			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void parseJsonString(String jsonString) {
+		JSONObject fullBase = new JSONObject(jsonString);
+		JSONArray atletasArray = fullBase.getJSONArray("atletas");
+		JSONObject firstAtleta = atletasArray.getJSONObject(0);
+		System.out.println(firstAtleta.toString());
+	}
+	
+	public static void main(String[] args) {
+		AthleteGenerator generator = new AthleteGenerator();
+		
+		generator.setBaseFullPath("/home/gilberto/Projects/SemNomeAindaCartolaFC/data/25-09-2016.json");
+		generator.extractMinAndMaxFromBase();
+	}
+
 }
